ce0875f4cda1213d0fe9111319cb80717e6565c3
H8er <nik.veras@mail.ru>
2021-01-20 16:12:01 +0300

    made buttons and classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,5 @@
 import './lib/lib'
 
-$('.el').addAttr('data-id')
-$('.active').click(sayHi)
-function sayHi(){
-    console.log('hi')
-}
-$('button').on('click', function (){
-    $('.more').fadeOut(1800)
+$('#first').on('click', () =>{
+    $('.container div').eq(0).fadeOut(800)
 })
-$('div').click(function (){
-    console.log($(this).index())
-})
-
-console.log($('.cool').find('.some'))
-
-console.log($('.more').eq(0).siblings())
-
-
