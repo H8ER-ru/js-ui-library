9ce8a95aa699d65b3d94a5f988ec41dd29eeffdb
H8er <nik.veras@mail.ru>
2021-01-22 11:25:00 +0300

    made dynamic modals

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,29 @@ import $ from "./lib/core";
 $('#first').on('click', () =>{
     $('.container div').eq(0).fadeToggle(800)
 })
+$('#trigger').click(() => {
+    $('#trigger').createModal({
+        text: {
+            title: 'Modal Title',
+            body: 'Aliquam aut delectus eius error facere hic impedit in, inventore iust molestiae mollitia',
+        },
+        btns : {
+            count: 2,
+            settings: [
+                [
+                    'Close',
+                    ['btn-danger', 'mr10'],
+                    true
+                ],
+                [
+                    'Save changes',
+                    ['btn-success'],
+                    false,
+                    () => {
+                        alert(' data saved')
+                    }
+                ]
+            ]
+        }
+    })
+})
